8969ea5861115ca0b1e365e64d2df0f9d2a7dbcf
Arine <peppydays@gmail.com>
2024-01-28 01:31:21 +0900

    Added several tests for confirmation

--- subscription/interface/api/src/handlers/confirmation.rs
+++ subscription/interface/api/src/handlers/confirmation.rs
@@ -1,15 +1,9 @@
 use std::sync::Arc;
 
 use anyhow::Context;
-use axum::extract::{
-    Query,
-    State,
-};
+use axum::extract::{Query, State};
 use axum::http::StatusCode;
-use domain::prelude::{
-    SubscriberRepository,
-    SubscriptionTokenRepository,
-};
+use domain::prelude::{SubscriberRepository, SubscriptionTokenRepository};
 
 use crate::error::ApiError;
 
@@ -65,9 +59,13 @@ pub async fn handle(
 #[cfg(test)]
 mod tests {
     use domain::prelude::{
-        MockSubscriberRepository,
-        MockSubscriptionTokenRepository,
+        MockSubscriberRepository, MockSubscriptionTokenRepository, Subscriber, SubscriptionToken,
     };
+    use fake::{
+        faker::{internet::en::SafeEmail, name::en::FirstName},
+        Fake,
+    };
+    use uuid::Uuid;
 
     use super::*;
 
@@ -96,5 +94,79 @@ mod tests {
         // then
         assert!(response.is_err());
         assert_eq!(response.unwrap_err().code, StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn confirmation_with_existing_token_without_subscriber_returns_not_found() {
+        // given
+        let mut subscriber_repository = MockSubscriberRepository::new();
+        let mut subscription_token_repository = MockSubscriptionTokenRepository::new();
+
+        subscription_token_repository
+            .expect_find_by_token()
+            .once()
+            .returning(|_| Ok(Option::Some(SubscriptionToken::issue(Uuid::new_v4()))));
+
+        subscriber_repository
+            .expect_find_by_id()
+            .once()
+            .returning(|_| Ok(Option::None));
+
+        // when
+        let request = Request {
+            token: "existing-token".to_string(),
+        };
+        let response = handle(
+            State(Arc::new(subscriber_repository)),
+            State(Arc::new(subscription_token_repository)),
+            Query(request),
+        )
+        .await;
+
+        // then
+        assert!(response.is_err());
+        assert_eq!(response.unwrap_err().code, StatusCode::NOT_FOUND);
+    }
+
+    #[tokio::test]
+    async fn confirmation_with_existing_token_and_subscriber_returns_ok() {
+        // given
+        let mut subscriber_repository = MockSubscriberRepository::new();
+        let mut subscription_token_repository = MockSubscriptionTokenRepository::new();
+
+        let subscriber_id = Uuid::new_v4();
+
+        subscription_token_repository
+            .expect_find_by_token()
+            .once()
+            .returning(move |_| Ok(Option::Some(SubscriptionToken::issue(subscriber_id))));
+
+        subscriber_repository
+            .expect_find_by_id()
+            .once()
+            .returning(move |_| {
+                Ok(Option::Some(
+                    Subscriber::new(subscriber_id, SafeEmail().fake(), FirstName().fake()).unwrap(),
+                ))
+            });
+
+        subscriber_repository
+            .expect_save()
+            .once()
+            .returning(|_| Ok(()));
+
+        // when
+        let request = Request {
+            token: "existing-token".to_string(),
+        };
+        let response = handle(
+            State(Arc::new(subscriber_repository)),
+            State(Arc::new(subscription_token_repository)),
+            Query(request),
+        )
+        .await;
+
+        // then
+        assert!(response.is_ok());
     }
 }
